f849548256c51afab9885f3f58cf9e65d96886e5
oldzhg <zcy000321@gmail.com>
2019-09-20 19:50:01 +0800

    Formatted code

--- AWS_Goods/write.py
+++ AWS_Goods/write.py
@@ -1,34 +1,33 @@
-from openpyxl import load_workbook
-import requests
 import re
+import requests
+from openpyxl import load_workbook
 
-base_url = 'https://www.amazon.ae/s?k='
-
+t = 1  # 开始位置
+p = 10  # 结束位置  汇总表中数据量共1w
+base_url = 'https://www.amazon.ae/s?k=' 
 filename = '汇总.xlsx'
+
 wb = load_workbook(filename)  # 打开一个工作簿
 sheet = wb['Sheet1']  # 获取表
-pattern = re.compile('">AED\s(.*?)</span>')
-t = 2501
-p = 2510
-for i in sheet.iter_rows(min_row=t, max_row=p):
+pattern = re.compile('">AED\s(.*?)</span>')  # 匹配到价格
 
+for i in sheet.iter_rows(min_row=t, max_row=p):  # 从t行到p行迭代
     good_id = i[0].value
-    good_id = str(good_id)
-    res = requests.get(base_url + good_id)
+    res = requests.get(base_url + str(good_id)) 
     good = re.findall(pattern, res.content.decode())
-    try:
-        if good:
-            pos = 'B' + str(t)
+    if good:  # 如果商品存在
+        pos = 'B' + str(t)
+        try:
             sheet[pos].value = float(good[0])  # 把价格写入到对应的地方
-            print("已提取 %d " % t)
-            wb.save('汇总.xlsx')
-        else:
-            pos = 'B' + str(t)
-            sheet[pos].value = ''  # 价格为空
-            print("已提取 %d " % t)
-            wb.save('汇总.xlsx')
-    except:
-        print(t)
+        except:
+            sheet[pos].value = good[0]
+        print("已提取 %d " % t)
+        wb.save('汇总.xlsx')
+    else:
+        pos = 'B' + str(t)
+        sheet[pos].value = ''  # 价格为空
+        print("已提取 %d " % t)
+        wb.save('汇总.xlsx')
     t = t + 1
 
-print("文件保存完成！")
+print("表格保存完成！")
